e0f464c8b5415c6946a38fb64dc7c7ce6745a3a6
marcelmarais <marcelmarais2000@gmail.com>
2021-05-29 20:54:08 +0100

    Final, final vis :)

--- app.py
+++ app.py
@@ -17,16 +17,24 @@ master_df = pd.read_csv('Data/master.csv')
 stats_df = master_df.groupby('SVI Category', as_index = False).median()
 
 vulnerability_order = ["Very High Vulnerability","High Vulnerability", "Moderate Vulnerability", "Low Vulnerability","Very Low Vulnerability"]
-vulnerability_colours = ["orange","blue","yellow","red","green"]
+vulnerability_colours = ["orange","blue","#fdfd96","#ff6961","green"]
 
 hesitant_vs_SVI = go.Bar(x=stats_df["SVI Category"], y=stats_df["Estimated hesitant"], marker_color=vulnerability_colours)
 
 fig1 = go.Figure(hesitant_vs_SVI)
 
 fig2_data = master_df.copy()
-fig2_data = fig2_data.groupby('State', as_index = False).mean()
+fig2_data = fig2_data.groupby('State', as_index = False).median()
 fig2 = px.scatter(fig2_data, x= "Estimated hesitant" , y = "cases", color = "State")
+
+fig4 = px.scatter(fig2_data, x= "Estimated hesitant" , y = "deaths", color = "State")
+
 fig2.update_traces(marker=dict(size=10,
+                              line=dict(width=2,
+                                        color='DarkSlateGrey')),
+                  selector=dict(mode='markers'))
+
+fig4.update_traces(marker=dict(size=10,
                               line=dict(width=2,
                                         color='DarkSlateGrey')),
                   selector=dict(mode='markers'))
@@ -40,6 +48,13 @@ fig1.update_layout(xaxis={'categoryorder':'array', 'categoryarray':vulnerability
 
 fig2.update_layout(xaxis={'categoryorder':'array', 'categoryarray':vulnerability_order},title={
         'text': "Number of COVID-19 Cases vs. Estimated Vaccine Hesistance by State.",
+        'y':0.9,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'})
+
+fig4.update_layout(xaxis={'categoryorder':'array', 'categoryarray':vulnerability_order},title={
+        'text': "Number of COVID-19 Deaths vs. Estimated Vaccine Hesistance by State.",
         'y':0.9,
         'x':0.5,
         'xanchor': 'center',
@@ -60,6 +75,8 @@ fig3.update_traces(marker=dict(size=10,
                   selector=dict(mode='markers'))   
 app.layout = html.Div(children=[
     html.H1(children='Exploring Vaccine Hesitancy in the United States', style={'text-align':'center'}),
+    html.Hr(),
+    html.H2(children='Social Vulnerability?', style={'text-align':'center'}),
     dcc.Graph(
         id='hesitant_vs_SVI',
         figure=fig1,
@@ -68,9 +85,15 @@ app.layout = html.Div(children=[
         id='sc',
         figure=fig3,
     ),
+    html.H2(children='Cases and Deaths?', style={'text-align':'center'}),
+
       dcc.Graph(
         id='scatter',
         figure=fig2,
+    ),
+      dcc.Graph(
+        id='scatter4',
+        figure=fig4,
     ),
 ])
 
